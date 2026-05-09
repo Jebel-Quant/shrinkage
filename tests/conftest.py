9404cbc87acd7b3f4cff9606c1ac6d9121373216
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2026-05-09 08:43:09 +0400

    rhiza framework

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+"""Fixtures for tests.
+"""
+
+from pathlib import Path
+
+import polars as pl
+import pytest
+
+
+@pytest.fixture(scope="session")
+def resource_dir():
+    """Fixture that provides the path to the test resources directory."""
+    return Path(__file__).parent / "resources
